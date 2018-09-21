ac1e9872d624fefcee304bf29b21795d71ad4511
Максим Царицинский <maksimcaricinskij@MacBook-Pro-Maksim.local>
2018-09-21 23:28:47 +0300

    next step

--- main.js
+++ main.js
@@ -1,63 +1,126 @@
 (function() {
+
+    var screenPoints = {
+        medium: 1200,
+        small: 768
+    }
+    // screen sizes
+    var device = checkScreen(screen.offsetWidth || screen.availWidth)
+    function checkScreen(screenWidth) {
+        if (screenWidth > screenPoints.medium) {
+            return {screen: 'desktop', imgCount: 4}
+        } else if (screenWidth <= screenPoints.medium && screenWidth > screenPoints.small) {
+            return {screen: 'tablet', imgCount: 3}
+        }
+        return {screen: 'mobile', imgCount: 2}
+    }
+
+    var carouselWrap = document.querySelector('.carousel')
+    var carousel = document.querySelector('.carousel-list')
     var imgItemList = document.querySelectorAll('.carouesel-img-item')
+    var carouselWidth = carousel.offsetWidth
+    
+
+    // default indent between images
+    var padding = 20
+    // set width for images
+    imgItemList.forEach((item, i) => {
+        if (i < device.imgCount) {
+            item.classList.add('active')
+        }   
+
+        // check device and set img count
+        // var deviceImgCount = 
+
+        item.style.width = (carousel.offsetWidth - padding * (device.imgCount - 1)) / device.imgCount + 'px'
+    })
+
     var imgItem = imgItemList[0]
-    var lastItem = imgItemList[imgItemList.length - 1]
-    var cloneLastItem = lastItem.cloneNode(true)
+
     // image width and step for scroll
     var step = imgItem ? imgItem.offsetWidth : null
     // endpoint
     var current = 0
     var currentSlide = 0
-    // default indent between images
-    var padding = 20
+     
+    var fullStep = step + padding
     // count of images
     var imgCount = imgItemList.length
 
-    var carousel = document.querySelector('.carousel-list')
-    // setTimeout(() => {
-    //     carousel.insertBefore(cloneLastItem, imgItem)
+    var currentWidth = 0
+    var fullWidth = fullStep * imgCount
 
-    // }, 2000)
-    carousel.style.transform = 'translate3d(' + 0 + 'px, 0px, 0px)'
-    var firstClone = imgItem
-
-    // buttons
+    // controls
     var prev = document.querySelector('.arrow-left')
     var next = document.querySelector('.arrow-right')
 
+    // --- left button (prev)
     prev.addEventListener('click', () => {
-        currentSlide += 1
+        var last = document.querySelector('.carousel-list img:last-child')
 
-        console.log(currentSlide, imgCount, currentSlide === imgCount)
-        if (currentSlide >= imgCount - 3) {
-            currentSlide = imgCount - 3
+        if (last.classList.contains('active')) {
             return
         }
-        current -= step + padding
-        carousel.style.transform = 'translate3d(' + current + 'px, 0px, 0px)'
-    })
 
+        currentSlide += 1
+        var index = device.imgCount
+        imgItemList.forEach((item, i) => {
+            item.classList.remove('active')
+            if (i >= currentSlide && index) {
+                item.classList.add('active')
+                index--
+            }
+        })
+
+        current -= fullStep
+        carousel.style.transform = 'translate3d(' + current + 'px, 0px, 0px)'
+        
+    })
+    // ---
+
+    // --- right button (next)
     next.addEventListener('click', () => {
-        currentSlide -= 1
-        console.log(currentSlide, imgCount, currentSlide === imgCount)
-        if (currentSlide <= 0) {
-            return currentSlide = 0
+        var first = document.querySelector('.carousel-list img:first-child')
+
+        if (first.classList.contains('active')) {
+            return
         }
 
-        // if (currentSlide < 0) {
-        //     var last = document.querySelector('.carousel-list img:last-child')
-        //     console.log('last', last)
-        //     var first = document.querySelector('.carousel-list img:first-child')
-        //     carousel.insertBefore(last, first)
-        // }
+        currentSlide -= 1
 
-        current += step + padding
-        console.log('current', currentSlide)
+        var index = device.imgCount
+        imgItemList.forEach((item, i) => {
+            item.classList.remove('active')
+
+            if (i >= currentSlide && index) {
+                item.classList.add('active')
+                index--
+            }
+        })
+        current += fullStep
+
         carousel.style.transform = 'translate3d(' + (current) + 'px, 0px, 0px)'
        
     })
-   
+    // ---
+
+    // --- responsible on resize
+    window.addEventListener('resize', () => {
+        device = checkScreen(screen.offsetWidth || screen.availWidth)
+        initWithActiveClass(imgItemList)
+    })
+    // ---
+    
+
+    function initWithActiveClass(list) {
+        list.forEach((item, i) => {
+            item.classList.remove('active')
+            if (i < device.imgCount) {
+                item.classList.add('active')
+            }
+
+            item.style.width = (carousel.offsetWidth - padding * (device.imgCount - 1)) / device.imgCount + 'px'
+        })
+    }
+
 })()
-
-
-// сделать клон картинки в конец и удалять из начала
